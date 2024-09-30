33eba88c3be1a5a81ecec429114956f5b792bf47
Ashutosh Apurva <apooorva01@gmail.com>
2024-09-30 17:30:35 +0000

    add ks8 network model

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # k8s-networking-101
 This repo consist of k8s networking practical learnings.
+
+## K8s Cluster Configuration 
+
+### Prerequisite for Practical demo
+ 1. K8s cluster setup with Multinode 
+ 2. kubectl command line tool
+ 3. Patience for Learning
+
+
+
+ 
---
+++ k8s_network_model/command_used.sh
@@ -0,0 +1,48 @@
+
+# Create the pod with multi container 
+kubectl create -f multi-container-pods.yml
+
+# Create the pod single container
+kubectl create -f single-container-pod.yml
+
+# Check the nodes details in pods
+kubectl get pod
+kubectl describe pod <pod_name>
+
+# Check the node where the pods are running aneg get the IP address of the worker nodes to SSH
+kubectl get nodes -o yaml
+
+ssh <IP address of the worker node>
+
+# Now execute the below commands step by step
+
+
+## Check the Linux NAMESPACES in the nodes of k8s cluster
+lsns -t pid
+
+#Check the output, here you can see the containers running inside the specific nodes with specific "pid"
+
+# To check the Network namespaces these containers belongs to 
+ip netns identify <pid of the container>
+
+# You will get output which have network plugin prefix( something like - "cni-80h8f89d78dc09c0ff8")
+
+# Now check the IP interface running in the nodes
+
+ip add 
+
+# Find the Virtual Interface (veth)
+
+ip add | grep -A1 veth
+
+# NOw again list the Linux namespace with pid
+
+lsns -t pid
+
+# Now find the namespae of the containers running in the node 
+
+for i in 2048 2068; do ip netns identify $i; done
+
+# The above command will list the network namespace id with network plugin prefix, if two container are running inside same pod then there namespace id aill be same.
+
+
